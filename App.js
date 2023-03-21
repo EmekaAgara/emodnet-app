8eaa79ddbed3849aaf7390d6016777ab9be7bf9f
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-03-21 09:02:05 +0100

    splash screen 1

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import WebView from 'react-native-webview';
+import * as React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import SplashScreen from './screens/SplashScreen';
+
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator>
+
+        <Stack.Screen
+            name='SplashScreen'
+            component={SplashScreen}
+            options={{
+            headerShown:false,
+            }}
+          />
+
+        </Stack.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/SplashScreen.js
@@ -0,0 +1,120 @@
+import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const SplashScreen = () => {
+
+    const navigation = useNavigation();
+
+    const onGetStartedPress = () => {
+      navigation.navigate('WalletType');
+    };
+
+    const onNextPress = () => {
+      navigation.navigate('SplashScreen2');
+    };
+
+
+  return (
+    
+    <View style={styles.container}>
+        <Image style={styles.image}
+            source={require('../assets/Square.png')}
+        />
+    <View style={styles.textcontainer}>
+    <Text style={styles.mainText}>Eliminating Polution</Text>
+    <Text style={styles.subText}>Eliminating pollution of our seas, waters, and Oceans by rewarding campaign participants</Text>
+    </View>
+        <TouchableOpacity onPress={onNextPress} style={styles.SecButtonContainer}>
+            <Text style={styles.ButtonText}>Next</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={onGetStartedPress} style={styles.ButtonContainer}>
+            <Text style={styles.ButtonText}>Get Started</Text>
+        </TouchableOpacity>
+    
+    </View>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+    },
+
+    textcontainer: {
+        backgroundColor: '#fff',
+        // alignItems: 'left',
+    },  
+
+    mainText:{
+        color:'black',
+        fontSize:22,
+        fontWeight:700,
+        textAlign:'left',
+        paddingBottom:10,
+        paddingTop:20,
+        paddingHorizontal:'8%',
+    },
+
+    subText:{
+        color:'white',
+        fontSize:13,
+        fontWeight:500,
+        textAlign:'left', 
+        paddingHorizontal:'8%',
+        color:'#818589',
+        lineHeight:20,
+        paddingBottom:20,
+    },
+
+    ButtonContainer: {
+        backgroundColor: "#2699E6",
+        borderRadius: 5,
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        width:'85%'
+
+    },
+
+    SecButtonContainer: {
+      backgroundColor: "#E63B60",
+      borderRadius: 5,
+      paddingVertical: 20,
+      paddingHorizontal: 140,
+      width:'85%',
+      marginBottom:10,
+    },
+
+
+    TetButtonContainer: {
+        borderWidth:0.5,
+        borderColor:'white',
+        borderRadius: 5,
+        paddingVertical: 20,
+        paddingHorizontal: 140,
+        width:'85%',
+        marginBottom:10,
+    },
+      
+
+
+    ButtonText: {
+        fontSize: 15,
+        color: "#fff",
+        fontWeight: 500,
+        alignSelf: "center",
+    },
+
+    image:{
+        width:'100%',
+        height:'68%',
+        resizeMode:'cover'
+    }
+
+
+  });
