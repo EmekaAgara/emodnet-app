1672deefaf2cea7b76cf1dc732515ea0ea5a2a9e
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-03-24 09:43:03 +0100

    campaigns screen done

--- components/Campaign.js
+++ components/Campaign.js
@@ -1,36 +1,20 @@
 import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
 
-const Campaign = () => {
+const Campaign = (props) => {
+
+  const campaign = props.campaign;
   return (
     <View style={styles.container}>
-      {/* Image */}
       <Image
         style={styles.image}
         source={{uri:'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1746&q=80'}}
       />
 
-      <Text style={styles.textcontainer}>
-        <Text style={styles.midText}>Category</Text>
-        <Text style={styles.description}>Location</Text>
-      </Text>
-
-      <Text style={styles.textcontainer}>
-        <Text style={styles.Maintext}>Title</Text>
-        <Text style={styles.midText}>date</Text>
-      </Text>
-      
-      <Text style={styles.description}>description</Text>
-
-      <Text style={styles.textcontainer}>
-        <Text style={styles.Maintext}>Donations raised</Text>
-        <Text style={styles.midText}>donations needed</Text>
-      </Text>
-      
-      <Text style={styles.midText}>participants</Text>
-      <Text style={styles.midText}>owner</Text>
-
-
+      <Text style={styles.textcontainer}>{post.category}Sea Bed Litter Cleaning</Text>
+      <Text style={styles.Maintext}>{post.title}Expedition spain july 26th</Text>   
+      <Text style={styles.description}>{post.description}Join us in cleaning up the sea bed litter in june in cleaning up the sea bed litter in june ...</Text>
+      <Text style={styles.midText2}>{post.raised}$100 Raised - 500 participants</Text>
 
     </View>
   )
@@ -40,14 +24,32 @@ export default Campaign
 
 const styles = StyleSheet.create({
 
-    image:{
-        width:'100%',
-        aspectRatio:3/2,
-        resizeMode:'cover',
-        borderRadius:7,
+  image:{
+    width:'100%',
+    aspectRatio:3/1.6,
+    resizeMode:'cover',
+    borderRadius:7,
+  },
 
-    }
+  Maintext:{
+    fontWeight:700,
+    fontSize:18
+  },
 
+  midText2:{
+    fontWeight:500,
+    paddingBottom:10
+  },
 
+  textcontainer:{
+    paddingVertical:5,
+    fontWeight:700,
+    color:'#528CFD'  
+  },
+
+  description:{
+    paddingVertical:5,
+    color:'gray'
+  },
 
 })
